ce705f010f858b4fc80e35d2f2268871382c2fdd
Kauan Victor <kauanvictor099@gmail.com>
2023-06-26 23:44:16 -0300

    Feita a Página Home

--- src/components/Imagens.jsx
+++ src/components/Imagens.jsx
@@ -1,13 +1,23 @@
 import forza from '../img/FORZA.jpg';
 import forza2 from '../img/FORZA2.jpg';
+import forza3 from '../img/forza3.jpg';
 import st from '../img/ST.jpg';
 import st2 from '../img/ST2.jpg';
 import valorant from '../img/VALORANT.jpg';
+import payday from '../img/payday.jpg'
+import fifa1 from '../img/fifa1.jpg'
+import halo from '../img/halo.jpg'
+import mario from '../img/mario.jpg'
 
 export const imagens = {
   forza,
   forza2,
+  forza3,
   st,
   st2,
   valorant,
+  payday,
+  fifa1,
+  halo,
+  mario,
 };
